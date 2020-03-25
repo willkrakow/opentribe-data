c923b380a188bbc640a369cce5f8f96a800e62ce
William Krakow <willkrakow@gmail.com>
2020-03-25 11:28:33 -0400

    Notes and project resources files

---
+++ data.js
@@ -0,0 +1,189 @@
+/*
+  Add any fields or nested data structures that you want.
+*/
+
+exports.account_info_credentials = {
+  // Medium.com login
+  // We could also use a webhook to post content via Zapier, which would make it easy to send an email to users when we post new blogs
+  // Hook URL to POST: https://hooks.zapier.com/hooks/catch/1899027/o1g72fd/
+
+  email: 'wkrakow@plantpurenation.om',
+  client_ID: '',
+  client_Secret: '',
+  access_token: '',
+  scope: '',
+  expires_at: ''
+  // ex: medium.com should have a PlantPure account
+  //     and i'll need the login/any available api keys, etc
+};
+
+exports.ideas_for_login_marketing = {
+
+};
+
+exports.general_content = {
+  home_page_banner_image: '',
+  browse_projects_banner_image: '',
+  link_to_plantpure_intro_video: '',
+  link_to_mission_video: ''
+};
+
+exports.remote_events = [
+  {
+    title: '',
+    start: '', // use a human readable date, best guess, or leave blank
+    description: '',
+    link: '',
+    link_to_album: ''
+  }
+];
+
+exports.projects = [
+  {
+    image_aspect: {
+      width: 3,
+      height: 1
+    },
+    resource_link: 'https://drive.google.com/open?id=1qLRk8dkrMUz9Qod6Z8OZejVEEnHptGne',
+    strategy_overview_link: 'https://drive.google.com/open?id=1Qu2edgWjGTOy-fdFaaX3zCyWU5UMV8Km',
+    cover_image: '',
+    title: 'Seminar Campaign',
+    video_link: ''
+  },
+  {
+    image_aspect: {
+      width: 3,
+      height: 1
+    },
+    resource_link: 'https://drive.google.com/open?id=14nGeqKGEUwg0pZsEYzl8yopzMHTKV4sI',
+    strategy_overview_link: 'https://drive.google.com/open?id=1-k1luR2hdGcaueFciezfdnpnFXuT5Exe',
+    cover_image: 'https://cdn.pixabay.com/photo/2017/07/23/16/01/nature-2531761_1280.jpg',
+    title: 'Pod Network',
+    video_link: ''
+  },
+  {
+    image_aspect: {
+      width: 3,
+      height: 1
+    },
+    resource_link: 'https://drive.google.com/open?id=1yszpzaeKGL9sm2AjlfcDQ1GjVSJmT3iZ',
+    strategy_overview_link: 'https://drive.google.com/open?id=1E_yNBLGxHGD_dURDK-fHiHPGRCf9utpG',
+    cover_image: 'https://images.pexels.com/photos/40568/medical-appointment-doctor-healthcare-40568.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
+    title: 'Healthcare Providers',
+    video_link: ''
+  },
+  {
+    image_aspect: {
+      width: 3,
+      height: 1
+    },
+    resource_link: 'https://drive.google.com/open?id=1GBX1yRF90Oy1nfEgAW6KNHKNpZfNOUaR',
+    strategy_overview_link: 'https://drive.google.com/open?id=1me6qKhXNdaiRPDT4l05rA79Cy-iX27nN',
+    cover_image: 'https://cdn.pixabay.com/photo/2015/01/08/18/26/write-593333_1280.jpg',
+    title: 'Employer Wellness',
+    video_link: ''
+  },
+  {
+    image_aspect: {
+      width: 3,
+      height: 1
+    },
+    resource_link: 'https://drive.google.com/open?id=1WjKFbX2ExDHAEuKvvU8PB4-Txvusye5T',
+    strategy_overview_link: 'https://drive.google.com/open?id=1QYMA_n3nxKAF17btICzVJgfpli8Q5ds9',
+    cover_image: 'https://cdn.pixabay.com/photo/2016/06/29/08/50/pencil-1486278_1280.jpg',
+    title: 'Schools and Hospitals',
+    video_link: ''
+  },
+  {
+    image_aspect: {
+      width: 3,
+      height: 1
+    },
+    resource_link: '',
+    strategy_overview_link: '',
+    cover_image: 'https://cdn.pixabay.com/photo/2017/09/16/19/21/salad-2756467_1280.jpg',
+    title: 'Restaurant Campaign',
+    video_link: ''
+  },
+  {
+    image_aspect: {
+      width: 3,
+      height: 1
+    },
+    resource_link: '',
+    strategy_overview_link: '',
+    cover_image: 'https://cdn.pixabay.com/photo/2015/12/01/20/28/green-1072828_1280.jpg',
+    title: 'Reforestation Campaign',
+    video_link: ''
+  }
+]
+
+exports.sponsors = [
+  {
+    image_aspect: {
+      width: 3,
+      height: 1
+    },
+    name: '',
+    description: '',
+    link: '',
+    cover_image: '',
+    link_to_album: ''
+  }
+];
+
+exports.media_links = [
+  {
+    image_aspect: {
+      width: 4,
+      height: 1
+    },
+    title: '',
+    description: '',
+    link: '',
+    cover_image: '',
+    link_to_album: ''
+  }
+];
+
+exports.media_announcements = [
+    {
+      image_aspect: {
+        width: 4,
+        height: 1
+      },
+      title: '',
+      description: '',
+      link: '',
+      cover_image: '',
+      link_to_album: ''
+    }
+  ];
+
+  exports.media_videos = [
+    {
+      image_aspect: {
+        width: 4,
+        height: 1
+      },
+      title: '',
+      description: '',
+      link: '',
+      cover_image: '',
+      link_to_album: ''
+    }
+  ];
+
+exports.blogs = [
+  {
+    image_aspect: {
+      width: 2,
+      height: 1
+    },
+    note: 'Ex. This should display on the home page banner left button',
+    title: '',
+    description: '',
+    link: '',
+    cover_image: ''
+  }
+];
