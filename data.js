3f90eca03be586a12a2bb22a23eca9366f2c0c79
William Krakow <willkrakow@gmail.com>
2020-03-27 15:44:11 -0400

    Video links

--- data.js
+++ data.js
@@ -25,7 +25,8 @@ exports.general_content = {
   home_page_banner_image: '',
   browse_projects_banner_image: '',
   link_to_plantpure_intro_video: '',
-  link_to_mission_video: ''
+  link_to_mission_video: 'https://youtu.be/vp5dJ-O-AQg',
+  link_to_opentribe_intro: 'https://youtu.be/wjOCXgUYvhU'
 };
 
 exports.remote_events = [
@@ -48,7 +49,7 @@ exports.projects = [
     strategy_overview_link: 'https://drive.google.com/open?id=1Qu2edgWjGTOy-fdFaaX3zCyWU5UMV8Km',
     cover_image: '',
     title: 'Seminar Campaign',
-    video_link: ''
+    video_link: 'https://youtu.be/jHV6nP-c_Dk'
   },
   {
     image_aspect: {
@@ -59,7 +60,7 @@ exports.projects = [
     strategy_overview_link: 'https://drive.google.com/open?id=1-k1luR2hdGcaueFciezfdnpnFXuT5Exe',
     cover_image: 'https://cdn.pixabay.com/photo/2017/07/23/16/01/nature-2531761_1280.jpg',
     title: 'Pod Network',
-    video_link: ''
+    video_link: 'https://youtu.be/UUTy9uWaHlQ'
   },
   {
     image_aspect: {
@@ -81,7 +82,7 @@ exports.projects = [
     strategy_overview_link: 'https://drive.google.com/open?id=1me6qKhXNdaiRPDT4l05rA79Cy-iX27nN',
     cover_image: 'https://cdn.pixabay.com/photo/2015/01/08/18/26/write-593333_1280.jpg',
     title: 'Employer Wellness',
-    video_link: ''
+    video_link: 'https://youtu.be/MKZbEdw1O1c'
   },
   {
     image_aspect: {
@@ -114,7 +115,29 @@ exports.projects = [
     strategy_overview_link: '',
     cover_image: 'https://cdn.pixabay.com/photo/2015/12/01/20/28/green-1072828_1280.jpg',
     title: 'Reforestation Campaign',
-    video_link: ''
+    video_link: 'https://youtu.be/WZsFNECR3xs'
+  },
+  {
+    image_aspect: {
+      width: 3,
+      height: 1
+    },
+    resource_link: '',
+    strategy_overview_link: '',
+    cover_image: '',
+    title: 'Oasis',
+    video_link: 'https://youtu.be/SVtSMXXQHZU'
+  },
+  {
+    image_aspect: {
+      width: 3,
+      height: 1
+    },
+    resource_link: '',
+    strategy_overview_link: '',
+    cover_image: '',
+    title: 'School Lunches',
+    video_link: 'https://youtu.be/M1UQpDNx22s '
   }
 ]
 
